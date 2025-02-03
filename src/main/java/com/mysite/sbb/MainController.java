a0d3f32fb928c37055b8ca4976ca380ab6ff1e7a
sangwoo <hun1246@hanmail.net>
2025-02-03 12:48:58 +0900

    가사수정

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,14 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        return "처음이라 그래 몇일뒤면 괜찮아져";
+    }
+}
